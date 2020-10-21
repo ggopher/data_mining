6305f0e64ac996121bcba113f3211a272e3e12f3
lapikit <ggopher@yandex.ru>
2020-10-21 13:40:19 +0300

    lesson2 homework

--- lesson2/homework2.py
+++ lesson2/homework2.py
@@ -10,7 +10,7 @@ class MagnitParser:
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0",
     }
     _params = {
-        'geo': 'sankt-peterbur',
+        'geo': 'moskva',
     }
 
     def __init__(self, start_url):
