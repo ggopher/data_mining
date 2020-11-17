f2fa552ce103f363a22b01fa17557ed5e1e88037
lapikit <ggopher@yandex.ru>
2020-11-18 00:47:13 +0300

    les8_hw

--- lesson9/instaparser/runner.py
+++ lesson9/instaparser/runner.py
@@ -4,12 +4,17 @@ from scrapy.settings import Settings
 from instaparser.spiders.instagram import InstagramSpider
 from instaparser import settings
 
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
 
     users = ['go_chatbot', '_.the_lashes._']
-    process.crawl(InstagramSpider, users=users)
+    process.crawl(InstagramSpider, login=os.getenv('LOGIN'), enc_password=os.getenv('ENC_PASSWORD'))
 
     process.start()
